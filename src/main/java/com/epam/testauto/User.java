1977bb070ed5e48acbf4a61239eb19e869b48a6d
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-01-26 16:19:43 +0300

    Create new class User

---
+++ src/main/java/com/epam/testauto/User.java
@@ -0,0 +1,7 @@
+package com.epam.testauto;
+
+public class User {
+    public static final String USER_NAME  = "PITER CHAILOVSKII";
+    public static final String USER_LOGIN = "epam";
+    public static final String USER_PASS  = "1234";
+}
